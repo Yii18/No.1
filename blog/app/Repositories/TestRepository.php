199ec6ddfddf7fa404dc3f807ad6492e37dbac2a
meil.zhu <meil.zhu@hnair.com>
2018-06-25 18:27:49 +0800

    目录构建

---
+++ blog/app/Repositories/TestRepository.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Repositories;
+ 
+use App\Repositories\BaseRepository;
+ 
+class TestRepository extends BaseRepository
+{
+  
+ /**
+  * Specify Model class name
+  * 
+  * @return string
+  */
+ public function model()
+ {
+  //set model name in here, this is necessary!
+ }
+}
